744a072a5d2e3b5c1cdcc0c7c110442c0d93af6b
lohit <00lohit@gmail.com>
2021-08-28 11:29:58 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ToDoContainer from './components/toDoContainer';
+
+
+function App() {
+  return (
+    <div className="App">
+    <ToDoContainer/>
+    </div>
+  );
+}
+
+export default App;
